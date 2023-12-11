0560c531a167cdd24b66f2405d06b9dfb88d5ad2
samsepiol-fs <shukla.pritesh214@gmail.com>
2023-12-11 13:05:29 +0530

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from 'bcryptjs';
+import Listing from '../models/listing.model.js';
 
 export const test = (req, res) => {
     res.json({ message: 'Hello, world!' });
@@ -39,4 +40,17 @@ export const deleteUser = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const getUserLsitings = async (req, res, next) => {
+    if(req.user.id === req.params.id) {
+        try {
+            const listings = await Listing.find({ userRef: req.params.id });
+            res.status(200).json(listings);
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        return next(errorHandler(401, 'You can only view your own listings'));
+    }
+};
