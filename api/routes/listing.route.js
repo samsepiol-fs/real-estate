66978a230058f1b3daaaad5cc872b79db2d85c8d
samsepiol-fs <shukla.pritesh214@gmail.com>
2023-12-12 23:01:50 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createListing , deleteListing} from '../controllers/listing.controller.js';
+import { createListing , deleteListing, updateListing} from '../controllers/listing.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create',verifyUser, createListing);
-router.delete('/delete/:id', verifyUser, deleteListing)
+router.delete('/delete/:id', verifyUser, deleteListing);
+router.post('/update/:id', verifyUser, updateListing);
 export default router;
