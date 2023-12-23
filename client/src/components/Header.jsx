4046f4f12e5989bf05cae93d25342f3d3f464929
samsepiol-fs <shukla.pritesh214@gmail.com>
2023-12-23 20:21:39 +0530

    update header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,10 +27,12 @@ export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-                <span className='text-slate-500'>real</span>
-                <span className='text-slate-700'>Estate.com</span>
-            </h1>
+            <Link to={'/'}>
+                <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                    <span className='text-slate-500'>real</span>
+                    <span className='text-slate-700'>Estate.com</span>
+                </h1>
+            </Link>
             <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
                 <input 
                     type="text"
